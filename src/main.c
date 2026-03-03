36e2654485cef068cd714af23d5fb30626a638b8
Radu Chira <chiraradu2004@gmail.com>
2026-03-04 00:41:45 +0100

    Add Raylib submodule

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
-#include <stdio.h>
+#include <raylib.h>
 
-int main()
-{
-  puts("Hello, World!");
+int
+main() {
+    InitWindow(800, 600, "Hello!");
+    int currentFps = 60;
+    SetTargetFPS(currentFps);
+
+    while (WindowShouldClose() == false) {
+        PollInputEvents();
+
+        ClearBackground((Color){0});
+        BeginDrawing();
+        {
+            const char *fpsText = 0;
+            if (currentFps <= 0)
+                fpsText = TextFormat("FPS: unlimited (%i)", GetFPS());
+            else
+                fpsText = TextFormat("FPS: %i (target: %i)", GetFPS(), currentFps);
+            DrawText(fpsText, 10, 10, 20, DARKGRAY);
+        }
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
